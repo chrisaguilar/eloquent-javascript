4b79fb786a7c1dd10fd243059af0b310651ce8a2
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2016-09-18 13:19:38 -0400

    Finished 05-02, added files for rest of 05-*

---
+++ 05/02-motherChildAgeDifference.js
@@ -0,0 +1,15 @@
+const ancestry = JSON.parse(require('./ancestry'));
+
+let byName = {};
+ancestry.forEach(person => byName[person.name] = person);
+
+const average = arr => arr.reduce((a, b) => a + b) / arr.length;
+const averageDiff = arr => 
+  average(
+    arr
+      .filter(p => byName[p.mother])
+      .map(p => p.born - byName[p.mother].born)
+  );
+
+console.log(averageDiff(ancestry));
+
